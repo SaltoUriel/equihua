17038aff8f1653af55d914a3fc50f158f1ddacc2
Uriel Salto Orozco <salto901011@gmail.com>
2018-06-10 00:29:23 -0500

    Pagos

---
+++ modulos/pagar.php
@@ -0,0 +1,9 @@
+<h5>México</h5>
+<ul class="list-group">
+  <li class="list-group-item"><h6>Banamex: </h6> <br>5204-1653-7457-4790</li>
+</ul>
+<h5>Centroamérica</h5>
+<ul class="list-group">
+    
+  <li class="list-group-item"><h6>Clave Interbancaria Santarder: </h6> <br>0144-7020-0113-0438-59</li>
+</ul>
